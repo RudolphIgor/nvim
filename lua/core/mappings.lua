e77e1209cc1151cda278731b81015771b616a962
RudolphIgor <azazil47@gmail.com>
2024-11-29 15:53:22 +0700

    Настроена навигация и сплиты

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,4 +7,13 @@ vim.keymap.set('i', 'jj', '<Esc>')
 --Buffers
 vim.keymap.set('n', '<leader>w', ':w<CR>')
 
---Test
+--Navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+--Split
+vim.keymap.set('n', '|', ':vsplit<CR>')
+vim.keymap.set('n', '\\', ':split<CR>')
+
