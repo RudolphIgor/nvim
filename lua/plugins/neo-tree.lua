8a76ee40d8be5694a0c80e978e9896c645c61ad7
RudolphIgor <azazil47@gmail.com>
2024-12-03 16:37:02 +0700

    Add color theme

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,10 +3,26 @@ return {
 	branch = "v3.x",
 	dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+	config = function()
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "none"})
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "none"})
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = " ",
+					[vim.diagnostic.severity.HINT] = " ",
+				},
+			},
+		})
+		require("neo-tree").setup({
+			close_if_last_window = false,
+		})
+	end,
 }
 
 
