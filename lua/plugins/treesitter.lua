4dfce71fbcb8096b31222af20d31239bdb9a1b98
RudolphIgor <azazil47@gmail.com>
2024-12-04 08:45:08 +0700

    Add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		'nvim-treesitter/nvim-treesitter',
+		config = function ()
+			require('nvim-treesitter.configs').setup({
+				ensure_installed = {"lua", "vim", "vimdoc", "javascript", "markdown",},
+				highlight = {
+					enable = true,
+				},
+			})
+		end
+	}
+}
